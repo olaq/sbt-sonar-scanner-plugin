14545674da3c2a58f60ed5c42af2f499dc3067cb
olaq <olaq@users.noreply.github.com>
2017-06-08 00:32:25 +0200

    Provided minimum default functionality

--- src/main/scala/com/olaq/sbt/SonarScannerPlugin.scala
+++ src/main/scala/com/olaq/sbt/SonarScannerPlugin.scala
@@ -4,23 +4,35 @@ import java.util.Properties
 
 import org.sonarsource.scanner.api.{EmbeddedScanner, LogOutput}
 import sbt.{Def, _}
+import Keys._
+
+import scala.collection.JavaConversions
 
 object SonarScannerPlugin extends AutoPlugin {
 
   object autoImport {
-    val sonarProperties: SettingKey[Map[String, String]] = SettingKey("settings for sonar scanner")
-    val sonarScan: TaskKey[Unit] = TaskKey[Unit]("Runs sonar-scanner")
+    val sonarScan: TaskKey[Unit] = taskKey[Unit]("Runs sonar-scanner")
+    val printSonarProperties: TaskKey[Unit] = taskKey[Unit]("Prints Sonar properties for current project")
+    val sonarProperties: SettingKey[Map[String, String]] = settingKey[Map[String, String]]("Sonar Scanner properties")
   }
 
   import autoImport._
 
-  override lazy val projectSettings = Seq(
-    sonarScan := sonarScanTask.value
+  override def projectSettings = Seq(
+    sonarScan := sonarScanTask.value,
+    printSonarProperties := printSonarPropertiesTask.value,
+    sonarProperties := Map(
+      "sonar.host.url" -> "http://localhost:9000",
+      "sonar.projectKey" -> s"${organization.value}:${name.value}",
+      "sonar.projectVersion" -> version.value,
+      "sonar.sources" -> sourceDirectory.in(Compile).value.absolutePath
+    )
   )
 
   private lazy val sonarScanTask = Def.task {
-    println("Sonar scanner run")
-    val properties = mapProperties()
+    val properties = new Properties()
+    properties.putAll(JavaConversions.mapAsJavaMap(sonarProperties.value))
+
     val runner = EmbeddedScanner.create(new LogOutputImpl())
       .addGlobalProperties(properties)
       .setApp("sbt", "1.0")
@@ -31,13 +43,8 @@ object SonarScannerPlugin extends AutoPlugin {
     } finally runner.stop()
   }
 
-  def mapProperties(): Properties = {
-    // TODO: map properties properly
-    val properties: _root_.java.util.Properties = new Properties()
-    properties.setProperty("sonar.host.url", "http://localhost:9009")
-    properties.setProperty("sonar.projectKey", "PlaySmapleTODO")
-    properties.setProperty("sonar.sources", "app")
-    properties
+  private lazy val printSonarPropertiesTask = Def.task {
+    sonarProperties.value.foreach(p => println(p._1 + "=" + p._2))
   }
 
   class LogOutputImpl extends LogOutput {
