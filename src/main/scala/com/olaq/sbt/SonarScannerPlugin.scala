7347d0bec03d6e96afb73187f97058447656a482
olaq <olaq@users.noreply.github.com>
2018-02-14 22:18:04 +0100

    Cross build compatibility (#3)
    * bump to 1.3.0-SNAPSHOT
    
    * Ensuring Scala 2.10 compatibility

--- src/main/scala/com/olaq/sbt/SonarScannerPlugin.scala
+++ src/main/scala/com/olaq/sbt/SonarScannerPlugin.scala
@@ -6,7 +6,7 @@ import org.sonarsource.scanner.api.{EmbeddedScanner, LogOutput}
 import sbt.{Def, _}
 import Keys._
 
-import scala.collection.JavaConverters
+import scala.collection.JavaConverters._
 
 object SonarScannerPlugin extends AutoPlugin {
 
@@ -47,7 +47,7 @@ object SonarScannerPlugin extends AutoPlugin {
 
   private lazy val sonarScanTask = Def.task {
     val properties = new Properties()
-    properties.putAll(JavaConverters.mapAsJavaMap(sonarProperties.value))
+    properties.putAll(sonarProperties.value.asJava)
 
     val runner = EmbeddedScanner.create(new LogOutputImpl(sLog.value))
       .addGlobalProperties(properties)
